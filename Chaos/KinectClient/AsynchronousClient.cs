92bdfaaba2d7ba811f159138fd54302d45310bf7
Chi-Jui Wu <cjw.charleswu@gmail.com>
2014-10-15 22:36:17 +0100

    going to try using sockets

---
+++ Chaos/KinectClient/AsynchronousClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KinectClient
+{
+    class AsynchronousClient
+    {
+        private const string ip = "localhost";
+        private const int port = 11000;
+
+        public AsynchronousClient()
+        {
+
+        }
+
+        public void StartClient()
+        {
+            try
+            {
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            }
+        }
+    }
+}
