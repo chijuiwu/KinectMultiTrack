5d607eb883dae2cf2e3183dacf1e2b22dcfc4145
Charlito <cjw.charleswu@gmail.com>
2014-10-18 15:03:40 +0100

    rename project to Tiny and more work on sockets

---
+++ Tiny/KinectClient/KinectClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KinectSocket
+{
+    class KinectClient
+    {
+        private string host;
+        private int port;
+
+        public KinectClient(string host, int port)
+        {
+            this.host = host;
+            this.port = port;
+        }
+
+        public void Start()
+        {
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(this.host), this.port);
+            client.Connect(endPoint);
+            try
+            {
+                while (true)
+                {
+                    string skeletonHeader = "Skeleton";
+                    byte[] bytesSkeletonHeader = ascii.GetBytes(skeletonHeader);
+                    Console.WriteLine("Sending Skeleton...");
+                    client.Send(bytesSkeletonHeader, bytesSkeletonHeader.Length);
+                    Byte[] receiveBytes = client.Receive(ref endPoint);
+                    string receiveString = ascii.GetString(receiveBytes, 0, receiveBytes.Length);
+                    Console.WriteLine("Received: " + receiveString + " from: " + endPoint);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in Kinect Client...");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ Tiny/Tiny/KinectServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chaos
+{
+    class KinectServer
+    {
+        private int port;
+
+        public KinectServer(int port)
+        {
+            this.port = port;
+        }
+
+        public void Start()
+        {
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            UdpClient server = new UdpClient(this.port);
+            Console.WriteLine("Starting server @ " + this.port + "...");
+            while (true)
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, this.port);
+                byte[] receiveBytes = server.Receive(ref endPoint);
+                string receiveString = ascii.GetString(receiveBytes, 0, receiveBytes.Length);
+                Console.WriteLine("Received: " + receiveString + " from: " + endPoint);
+                string okay = "Okay";
+                byte[] sendBytes = ascii.GetBytes(okay);
+                server.Send(sendBytes, sendBytes.Length, endPoint);
+            }
+        }
+    }
+}
