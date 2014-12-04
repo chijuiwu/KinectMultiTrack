f21fc39807171f910e179220d98c5bb978b010ae
Chi-Jui Wu <cjw.charleswu@gmail.com>
2014-12-04 06:52:51 +0000

    remove unncessary null check

--- Tiny/Tiny/KinectServer.cs
+++ Tiny/Tiny/KinectServer.cs
@@ -100,10 +100,6 @@ namespace Tiny
                     while (!clientStream.DataAvailable) ;
 
                     SerializableBodyFrame bodyFrame = BodyFrameSerializer.Deserialize(clientStream);
-                    if (bodyFrame == null)
-                    {
-                        Debug.WriteLine("body frame is null");
-                    }
                     this.userTracker.AddOrUpdateBodyFrame(clientIP, bodyFrame);
                     Thread visualUpdateThread = new Thread(new ThreadStart(this.StartVisualUpdateThread));
                     visualUpdateThread.Start();
