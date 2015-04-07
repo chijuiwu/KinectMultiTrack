f1ff8fd11c3c250c680b6f4394f45df08247884a
Charlito <cjw.charleswu@gmail.com>
2015-04-07 14:53:22 +0100

    new report style

---
+++ Deliverables/Report/Final/run.bat
@@ -0,0 +1,13 @@
+pdflatex thesis
+bibtex thesis
+pdflatex thesis
+pdflatex thesis
+
+perl texcount.pl chapters/*.tex
+
+@echo off
+echo.
+echo.
+echo SH Project Report (report.pdf)
+echo Chi-Jui Wu 2015
+echo.
